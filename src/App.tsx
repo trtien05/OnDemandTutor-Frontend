320ef939c2788ff3540a2802940445b9838f8f1f
alicee-19 <anh2k4.neko@gmail.com>
2024-06-06 13:01:36 +0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponent from './routes';
 import BookTutor from './components/Modal/BookTutor/BookTutor';
+
 function App() {
 
   return (
     // <Router>
     //   <RoutesComponent />
     // </Router>
-    <BookTutor></BookTutor>
+        <BookTutor></BookTutor>
   )
 }
 
