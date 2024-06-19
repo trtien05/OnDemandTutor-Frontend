b54c8409ab7a58aa540116d355f7b599795f9548
trtien05 <trantien100700@gmail.com>
2024-06-19 14:55:14 +0700

    fix Ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { AntdThemeConfig } from './themes/';
 import viVN from 'antd/es/locale/vi_VN';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { StrictMode } from 'react';
 import GlobalStyles from './themes/globalStyles.ts';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
 import { createStyledBreakpointsTheme } from 'styled-breakpoints';
@@ -24,12 +24,15 @@ const theme: DefaultTheme = createStyledBreakpointsTheme({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <ConfigProvider theme={AntdThemeConfig}>
-      <AppAntd>
-        <App />
-      </AppAntd>
-      <GlobalStyles />
-    </ConfigProvider>
-  </ThemeProvider>
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <ConfigProvider locale={viVN} theme={AntdThemeConfig}>
+        <AppAntd>
+          <App />
+        </AppAntd>
+        <GlobalStyles />
+      </ConfigProvider>
+    </ThemeProvider>
+  </StrictMode>
+
 )
