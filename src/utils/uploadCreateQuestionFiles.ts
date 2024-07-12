fd60a22d047628e0616f25be77974fff574ed2b2
Giang Nguyen Thi Huong <nguyenthihuonggiangthth@gmail.com>
2024-07-12 19:29:52 +0700

    Fix create question file

--- src/utils/uploadCreateQuestionFiles.ts
+++ src/utils/uploadCreateQuestionFiles.ts
@@ -1,10 +1,8 @@
 import { getDownloadURL, uploadBytes, ref } from "firebase/storage";
 import { storage } from "../utils/firebase";
-// , handleChange: (url: string) => void
 export const uploadCreateQuestionFiles = async (userId: number, file: File, sectionName: string, dateCreated: string) => {
-    // console.log(file);
     if (!file) {
-        console.log("No file selected for upload.");
+        // console.log("No file selected for upload.");
         return;
     }
 
@@ -13,10 +11,7 @@ export const uploadCreateQuestionFiles = async (userId: number, file: File, sect
         const uploadResult = await uploadBytes(imageRef, file);
 
         const url = await getDownloadURL(uploadResult.ref);
-        console.log(`File available at: ${url}`);
-        // if (url) {
-        //     handleChange(url);
-        // }
+        // console.log(`File available at: ${url}`);
         return url;
     } catch (error) {
         console.log(`Upload failed: ${error}`);
