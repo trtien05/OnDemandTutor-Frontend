c3dc335aca8c3cbf86741ef6bc96eed6ea81fc8c
trtien05 <trantien100700@gmail.com>
2024-07-22 23:38:25 +0700

    delete hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,16 +1,6 @@
 import useAuth from './useAuth';
-import useWindowsDimension from './useWindowsDimension';
 import useScrollToTop from './useScrollToTop';
-import { useAppDispatch, useAppSelector } from './useStore';
 import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 import useDocumentTitle from './useDocumentTitle';
 
-export {
-    useScrollToTop,
-    useWindowsDimension,
-    useAuth,
-    useAppDispatch,
-    useAppSelector,
-    useIsomorphicLayoutEffect,
-    useDocumentTitle,
-};
+export { useScrollToTop, useAuth, useIsomorphicLayoutEffect, useDocumentTitle };
